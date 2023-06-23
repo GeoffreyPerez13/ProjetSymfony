963bc50da5c5e3b6b674bb5ffd2ff0ec8628c002
GeoffreyPerez13 <geoffrey.perez13400@hotmail.com>
2023-06-23 16:26:54 +0200

    Ajout affichage 2 lieux random

---
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use App\Repository\PlaceRepository;
+use Twig\Environment;
+
+class TwigEventSubscriber implements EventSubscriberInterface
+{
+    private $twig;
+    private $placeRepository;
+
+    public function __construct(Environment $twig, PlaceRepository $placeRepository)
+    {
+        $this->twig = $twig;
+        $this->placeRepository = $placeRepository;
+    }
+
+    public function onKernelController(ControllerEvent $event): void
+    {
+        $this->twig->addGlobal('randomPlaces', $this->placeRepository->findRandomPlaces(2));
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::CONTROLLER => 'onKernelController',
+        ];
+    }
+}
