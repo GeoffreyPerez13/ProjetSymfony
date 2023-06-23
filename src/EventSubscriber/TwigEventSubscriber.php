4c228b4299474f16d709c83d90aa0242396b82f0
cecilejeanneau <cecile.n.jeanneau@gmail.com>
2023-06-23 16:32:44 +0200

    ✨ add randomPlaces feature

---
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Repository\PlaceRepository;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Twig\Environment;
+
+
+class TwigEventSubscriber implements EventSubscriberInterface
+{
+
+    private $twig;
+    private $placeRepository;
+
+    public function __construct(Environment $twig, PlaceRepository $placeRepository)
+    {
+        $this->twig = $twig;
+        $this->placeRepository = $placeRepository;
+    }
+    public function onKernelController(ControllerEvent $event): void
+    {
+        $this->twig->addGlobal('randomPlaces', $this->placeRepository->findRandomPlaces(2));
+        // $this->twig->addGlobal('randomPlaces', $this->placeRepository->findAll());
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::CONTROLLER => 'onKernelController',
+        ];
+    }
+}
